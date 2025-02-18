4fdce9bb9e2bacc338337c01c7a108f9246d5546
iiqsyx <alex_iiqsyx@mail.ru>
2025-02-18 21:42:33 +0300

    feat: add fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,62 @@
+import fs from 'fs';
+import path from 'path';
+
+const checkingFolderExistence = async (path) => {
+    return new Promise((resolve, reject) => fs.access(path, (err) => {
+        if (err) {
+            return reject(new Error(`FS operation failed`));
+        } else {
+            return resolve();
+        }
+    }))
+}
+
+const mkdirAsync = async (dirPath) => {
+    return new Promise((resolve, reject) => fs.mkdir(dirPath, (err) => {
+        if (err) {
+            return reject(new Error(`FS operation failed`));
+        }
+
+        resolve();
+    }))
+}
+
+const readFolderAsync = async (dirPath) => {
+    return new Promise((resolve, reject) => fs.readdir(dirPath, (err, data) => {
+        if (err) {
+            return reject(error.message);
+        } else{
+            return resolve(data);
+        }
+    }))
+}
+
+const copyFileAsync = async (mainFilePath, copyFilePath) => {
+    return new Promise((resolve, reject) => fs.copyFile(mainFilePath, copyFilePath, (err) => {
+        if (err) {
+            return reject(err.message);
+        } else{
+            return resolve();
+        }
+    }))
+}
+
 const copy = async () => {
-    // Write your code here 
+    const mainFolderPath = './files';
+    const copyFolderPath = './files_copy';
+
+    checkingFolderExistence(mainFolderPath)
+        .then(() => mkdirAsync(copyFolderPath))
+        .then(() => readFolderAsync(mainFolderPath))
+        .then((data) => {
+            for (const file of data) {
+                const mainFilePath = path.join(mainFolderPath, file);
+                const copyFilePath = path.join(copyFolderPath, file);
+
+                copyFileAsync(mainFilePath, copyFilePath);
+            }
+        })
+        .catch(err => console.log(err));
 };
 
 await copy();
