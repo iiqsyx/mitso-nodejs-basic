43430f75476c58ff68e0a10f5733a0837d9c493f
iiqsyx <alex_iiqsyx@mail.ru>
2025-02-18 21:54:04 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+
+const checkingFileExistence = async (path) => {
+    return new Promise((resolve, reject) => fs.access(path, (err) => {
+        if (err) {
+            return reject(new Error(`FS operation failed`));
+        } else {
+            return resolve();
+        }
+    }))
+}
+
+const renameFileAsync = async (wrongFilePath, properFilePath) => {
+    return new Promise((resolve, reject) => fs.rename(wrongFilePath, properFilePath, (err) => {
+        if (err) {
+            return reject(new Error(`FS operation failed`));
+        }
+
+        resolve();
+    }))
+}
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFilePath = './files/wrongFilename.txt';
+    const properFilePath = './files/properFilename.md';
+
+    checkingFileExistence(wrongFilePath)
+        .then(() => renameFileAsync(wrongFilePath, properFilePath))
+        .catch(err => console.log(err));
 };
 
 await rename();
