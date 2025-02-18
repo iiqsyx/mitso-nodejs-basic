9886f9bf0535d052e8392d59c903e6145ad2c060
iiqsyx <alex_iiqsyx@mail.ru>
2025-02-18 22:07:25 +0300

    feat: add fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+
+const checkingFileExistence = async (path) => {
+    return new Promise((resolve, reject) => fs.access(path, (err) => {
+        if (err) {
+            return reject(new Error(`FS operation failed`));
+        } else {
+            return resolve();
+        }
+    }))
+}
+
+const removeFileAsync = async (path) => {
+    return new Promise((resolve, reject) => fs.rm(path, (error) => {
+        if (error) {
+            return reject(error.message);
+        }
+
+        resolve();
+    }))
+}
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = './files/fileToRemove.txt';
+
+    checkingFileExistence(filePath)
+        .then(() => removeFileAsync(filePath))
+        .catch(err => console.log(err));
 };
 
 await remove();
