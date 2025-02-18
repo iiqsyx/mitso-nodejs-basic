fecb38c99590bbfeecd39da48ec8c004a6089b9b
iiqsyx <alex_iiqsyx@mail.ru>
2025-02-18 20:39:56 +0300

    feat: add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+
+const checkingFileExistence = async (path) => {
+    return new Promise((resolve, reject) => fs.access(path, (err) => {
+        if (err) {
+            return resolve();
+        } else {
+            return reject(new Error(`FS operation failed`));
+        }
+    }))
+}
+
+const writeFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+        if (err) {
+            return reject(err.message);
+        }
+
+        resolve();
+    }))
+}
+
 const create = async () => {
-    // Write your code here 
+    const filePath = './files/fresh.txt';
+    const data = 'I\'m fresh and young';
+
+    checkingFileExistence(filePath)
+        .then(() => writeFileAsync(filePath, data))
+        .catch(err => console.log(err));
 };
 
 await create();
